a04c5bb108951ec5ffb1fcbf05d69a8163f8ad40
rijalwafi <rijalwafi69@gmail.com>
2024-01-11 17:54:33 +0700

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import ProductCard from './components/ProductCard'
+const App = () => {
+  return (
+    <div className="cards">
+      <ProductCard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,23 @@
+const ProductCard = () => {
+  return (
+    <div className="card">
+      <img
+        src="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        alt=""
+        style={{
+          width: '100%',
+          height: '200px',
+          borderRadius: '10px 10px 0px 10px',
+        }}
+      />
+      <div className="container">
+        <h4>
+          <b>Ford</b>
+        </h4>
+        <p>Mobil ford adalah mobil yang bagus</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
