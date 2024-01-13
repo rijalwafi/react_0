9717ea9652b23902d609455504ed677b626a240b
rijalwafi <rijalwafi69@gmail.com>
2024-01-14 05:08:59 +0700

    add all file changes today

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Products } from './data/Product'
 import ProductCard from './components/ProductCard'
+import ProductList from './components/ProductList'
 const App = () => {
+  // const [counter, setCounter] = useState(0)
+  // const decrement = () => {
+  //   setCounter(counter - 1)
+  //   console.log(counter)
+  // }
+  // const increment = () => {
+  //   setCounter(counter + 1)
+  //   console.log(counter)
+  // }
+
   return (
-    <div className="cards">
-      {Products.map((data, array) => (
-        <ProductCard
-          key={array}
-          name={data.nama}
-          deskripsi={data.deskripsi}
-          imageUrl={data.imageURL}
-        />
-      ))}
+    <div>
+      <div>
+        <div className='app-title'>Belanja Mobil</div>
+      </div>
+      <div>
+        <ProductList />
+      </div>
     </div>
   )
 }
